276f9f7a7d197df2ba7df80e1018a27ed2e5c3ef
prethush <ktmirash49@gmail.com>
2021-06-07 20:09:06 +0530

    added files

--- block-BJaaju/public/index.js
+++ block-BJaaju/public/index.js
@@ -1,40 +1,43 @@
 let imgContainer = document.querySelector(".img-container");
 let input = document.querySelector("input");
 
+const url = `https://api.unsplash.com/photos/?client_id=IBHskJXnOj3hIHuE42rGXHUqsvOUd4YI8aHXYH26js8`;
+const search_url = (query) => `https://api.unsplash.com/search/photos?&query=${query}&client_id=IBHskJXnOj3hIHuE42rGXHUqsvOUd4YI8aHXYH26js8`;
+
+function fetch(url, successhandler) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.onload = () => successhandler(JSON.parse(xhr.response));
+    xhr.onerror = function() {
+        console.log("Something went wrong");
+    }
+    xhr.send();
+}
+
+function displayImages(images) {
+    console.log(images);
+    imgContainer.innerHTML = "";
+    images.forEach((image) => {
+        let div = document.createElement("div");
+        div.classList.add("img-div");
+        div.style.background = `url(${image.urls.small}) center center no-repeat`;
+        div.style.backgroundSize = "cover";
+        div.style.height = "300px";
+        imgContainer.append(div);
+    })
+}
 
 
+fetch(url, displayImages);
 
 function handleChange(event) {
-
-
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", `https://api.unsplash.com/search/photos?page=1&query=${event.target.value}&client_id=IBHskJXnOj3hIHuE42rGXHUqsvOUd4YI8aHXYH26js8`);
-
-
-    xhr.onload = function() {
-        imgContainer.innerHTML = "";
-        let data = JSON.parse(xhr.response);
-    
-        let imageArray = JSON.parse(xhr.response).results;
-        imageArray.forEach((el) => {
-            
-            let div = document.createElement("div");
-            div.classList.add("img-div");
-            div.style.background = `url(${el.urls.small}) center center no-repeat`;
-            div.style.backgroundSize = "cover";
-            div.style.height = "300px";
-            imgContainer.append(div);
-            
+    if(event.keyCode === 13 && input.value) {
+        fetch(search_url(input.value), (searchresult) => {
+            displayImages(searchresult.results);
         })
-       
 
+        event.target.value = "";
     }
-
-    xhr.onerror = function() {
-        console.log("Something went Wrong");
-    }
-
-    xhr.send();
 
 }
-input.addEventListener("input", handleChange);
+input.addEventListener("keydown", handleChange);
