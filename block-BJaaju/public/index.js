3e8691959f612d0fc5325d719ef803dc33d92afe
prethush <ktmirash49@gmail.com>
2021-06-08 12:14:31 +0530

    added files

--- block-BJaaju/public/index.js
+++ block-BJaaju/public/index.js
@@ -5,17 +5,21 @@ const url = `https://api.unsplash.com/photos/?client_id=IBHskJXnOj3hIHuE42rGXHUq
 const search_url = (query) => `https://api.unsplash.com/search/photos?&query=${query}&client_id=IBHskJXnOj3hIHuE42rGXHUqsvOUd4YI8aHXYH26js8`;
 
 function fetch(url, successhandler) {
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
-    xhr.onload = () => successhandler(JSON.parse(xhr.response));
-    xhr.onerror = function() {
-        console.log("Something went wrong");
-    }
-    xhr.send();
+    return new Promise((res, rej) => {
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+        xhr.onload = () => res(JSON.parse(xhr.response));
+        xhr.onerror = () => rej(console.log("Something went wrong"))
+        xhr.send();
+    })
+    
 }
 
-function displayImages(images) {
-    console.log(images);
+
+
+
+let res = fetch(url)
+.then((images) => {
     imgContainer.innerHTML = "";
     images.forEach((image) => {
         let div = document.createElement("div");
@@ -25,16 +29,26 @@ function displayImages(images) {
         div.style.height = "300px";
         imgContainer.append(div);
     })
-}
-
-
-fetch(url, displayImages);
+})
+.catch((error) => alert(error));
 
 function handleChange(event) {
     if(event.keyCode === 13 && input.value) {
-        fetch(search_url(input.value), (searchresult) => {
-            displayImages(searchresult.results);
+        fetch(search_url(input.value))
+        .then((data) => {
+            let images = data.results;
+            imgContainer.innerHTML = "";
+            images.forEach((image) => {
+            let div = document.createElement("div");
+            div.classList.add("img-div");
+            div.style.background = `url(${image.urls.small}) center center no-repeat`;
+            div.style.backgroundSize = "cover";
+            div.style.height = "300px";
+            imgContainer.append(div);
+    })
         })
+
+    .catch((error) => alert(error));
 
         event.target.value = "";
     }
