23dae71b152eb703625f731b007f7f0de556a573
prethush <ktmirash49@gmail.com>
2021-06-13 10:35:25 +0530

    added files

--- block-BJaakg/public/index.js
+++ block-BJaakg/public/index.js
@@ -21,9 +21,11 @@ function displayCharacters(characters) {
     handleSpinner(charactersData, true);   
     Promise.all(characters.map((character) =>fetch(character).then((res) => res.json())))
     .then((data) => {
+        console.log(data);
         charactersData.innerHTML = "";
         data.forEach((character) => {
         let li = document.createElement("li");
+        li.classList.add("border-solid", "border-2", "border-white", "p-2", "my-2", "text-xl");
          li.innerText = `${character.name}: ${character.aliases.join(" ")}`;
         charactersData.append(li);
         
@@ -42,13 +44,16 @@ function displayUI(books) {
     ul.innerHTML = "";
     books.forEach((book) => {
         let li = document.createElement("li");
-        li.classList.add("flex-33");
+        li.classList.add("flex-50", "p-6", "border-dashed", "border-2", "border-red-500");
         let title = document.createElement("h2");
+        title.classList.add("text-2xl", "font-bold", "mb-5");
         title.innerText = book.name;
         let author = document.createElement("h3");
-        author.innerText = book.authors[0]
+        author.innerText = book.authors[0];
+        author.classList.add("text-xl", "mb-6");
         let button = document.createElement("a");
         button.innerText = `Show Characters ${book.characters.length}`;
+        button.classList.add("bg-black", "text-white", "py-2", "px-2", "rounded-xl", "cursor-pointer");
         button.addEventListener("click", (e) => {
             displayCharacters(book.characters);
             modal.style.display = "block";
@@ -69,6 +74,12 @@ function booksData() {
         
 booksData();
 // Promise.all(books.map((book) => book).then(console.log));
+
+modal.addEventListener("load", (e) => {
+    console.log("Hi");
+modal.style.display = "none";
+});
+console.log(modal);
  
 })(); 
 
